2a579e0ca05c4a61df5dd7827f8fd03ef0eca0bb
Rich Ellis <ricellis@users.noreply.github.com>
2018-02-15 16:57:15 +0000

    Fixed javadoc in ResourceBundleUtil

--- src/main/java/com/ibm/cloudant/kafka/common/utils/ResourceBundleUtil.java
+++ src/main/java/com/ibm/cloudant/kafka/common/utils/ResourceBundleUtil.java
@@ -1,29 +1,27 @@
-/*******************************************************************************
-* Copyright (c) 2016 IBM Corp.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+/*
+ * Copyright © 2016, 2018 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package com.ibm.cloudant.kafka.common.utils;
+
+import com.ibm.cloudant.kafka.common.InterfaceConst;
+
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
-
-import org.apache.log4j.Logger;
-
-import com.ibm.cloudant.kafka.common.InterfaceConst;
 
 public class ResourceBundleUtil {
 	
@@ -57,9 +55,10 @@ public class ResourceBundleUtil {
 	
 	/***
 	 * Generate topics to be consumed considering number of 
-	 * tasks 
-	 * @param maxTasks - max number of tasks
-	 * @return
+	 * tasks
+	 * @param configProperties the configuration properties from which to extract the list of topics
+	 * @param maxTasks max number of tasks
+	 * @return a list of topics
 	 */
 	public static ArrayList<String> balanceTopicsTasks(Map<String, String> configProperties, int maxTasks) {
 		// Get topics
