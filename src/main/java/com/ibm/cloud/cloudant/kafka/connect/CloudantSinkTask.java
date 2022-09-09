b72b8d385af8167b3fbd324a6c14b4fa0d18cf83
Tom Blench <Thomas.Blench@ibm.com>
2022-09-09 11:43:36 +0100

    PR feedback
    - Break write into smaller batches
    - Don't clear accumulated sink records on failure
    - Don't report all records on batch failure - they will be retried by
      connect
    - Remove unnecessary failure logging

--- src/main/java/com/ibm/cloud/cloudant/kafka/connect/CloudantSinkTask.java
+++ src/main/java/com/ibm/cloud/cloudant/kafka/connect/CloudantSinkTask.java
@@ -29,7 +29,7 @@ import org.apache.kafka.connect.sink.SinkTaskContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -43,7 +43,7 @@ public class CloudantSinkTask extends SinkTask {
 	
 	List<String> topics = null;
 
-	public static int batch_size = 0;
+	public static int batchSize = 0;
 	private int taskNumber;
 	public static String guid_schema = null;
 	private Boolean replication;
@@ -61,18 +61,12 @@ public class CloudantSinkTask extends SinkTask {
 		 return new CloudantSinkConnector().version();
 	}
 
-	//TODO: all sinkRecords in first Thread
 	@Override
 	public void put(Collection<SinkRecord> sinkRecords) {
-
 		if (accumulatedSinkRecords == null) {
 			accumulatedSinkRecords = new LinkedList<>();
 		}
-
-		System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
-
 		LOG.info("Thread[" + Thread.currentThread().getId() + "].sinkRecords = " + sinkRecords.size());
-
 		accumulatedSinkRecords.addAll(sinkRecords);
 	}
 
@@ -96,7 +90,7 @@ public class CloudantSinkTask extends SinkTask {
             //TODO: split topics from Connector
             topics = config.getList(InterfaceConst.TOPIC);
             
-            batch_size = config.getInt(InterfaceConst.BATCH_SIZE)==null ? InterfaceConst.DEFAULT_BATCH_SIZE : config.getInt(InterfaceConst.BATCH_SIZE);			
+            batchSize = config.getInt(InterfaceConst.BATCH_SIZE)==null ? InterfaceConst.DEFAULT_BATCH_SIZE : config.getInt(InterfaceConst.BATCH_SIZE);
 			replication = config.getBoolean(InterfaceConst.REPLICATION) == null ? InterfaceConst.DEFAULT_REPLICATION : config.getBoolean(InterfaceConst.REPLICATION); 
 		} catch (ConfigException e) {
 			throw new ConnectException(ResourceBundleUtil.get(MessageKey.CONFIGURATION_EXCEPTION), e);
@@ -106,11 +100,10 @@ public class CloudantSinkTask extends SinkTask {
 	@Override
 	public void flush(Map<TopicPartition, org.apache.kafka.clients.consumer.OffsetAndMetadata> offsets) {
 
-		System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+		List<Map<String, Object>> jsonArray = new ArrayList<>();
 
-		List<Map<String, Object>> jsonArray = new LinkedList<>();
-
-		if (accumulatedSinkRecords != null) {
+		if (accumulatedSinkRecords != null && !accumulatedSinkRecords.isEmpty()) {
+			LOG.info(String.format("flush called with %d documents to %s", accumulatedSinkRecords.size(), config.getString(InterfaceConst.URL)));
 			// Note: _rev is preserved
 			accumulatedSinkRecords.stream()
 					.map(mapper) // Convert ConnectRecord to Map
@@ -118,38 +111,44 @@ public class CloudantSinkTask extends SinkTask {
 					.forEach(recordValueAsMap -> {
 						jsonArray.add(recordValueAsMap);
 					});
-		}
-
-
-		try {
-			LOG.info(String.format("Calling batchWrite with %d documents to %s", jsonArray.size(), config.getString(InterfaceConst.URL)));
-			List<DocumentResult> writeResults = JavaCloudantUtil.batchWrite(config.originalsStrings(), jsonArray);
-			boolean someFailed = writeResults.stream().anyMatch(doc -> doc.isOk() == null || !doc.isOk());
-			if (someFailed) {
-				LOG.error("Failed to write some documents");
-				for (int i=0; i<writeResults.size(); i++) {
-					// TODO - is checking isOk sufficient?
-					// TODO - are bulk doc results guaranteed to be in same order?
-					// TODO - think of a better exception to raise here
-					if (!writeResults.get(i).isOk()) {
-						reporter.report(accumulatedSinkRecords.get(i), new RuntimeException("not ok"));
+			try {
+				// break down accumulated records into batches to send to cloudant
+				// NB a failure in _any_ batch will currently cause all accumulated records to be marked as uncommitted
+				int nBatches = jsonArray.size() / batchSize + (jsonArray.size() % batchSize == 0 ? 0 : 1);
+				LOG.info(String.format("flush called with %d batches to %s", nBatches, config.getString(InterfaceConst.URL)));
+				for (int b = 0; b < nBatches; b++) {
+					List<Map<String, Object>> jsonSubArray = jsonArray.subList(b * batchSize, Math.min(jsonArray.size(), (b + 1) * batchSize));
+					LOG.info(String.format("Calling batchWrite with %d documents to %s", jsonSubArray.size(), config.getString(InterfaceConst.URL)));
+					List<DocumentResult> writeResults = JavaCloudantUtil.batchWrite(config.originalsStrings(), jsonSubArray);
+					boolean someFailed = writeResults.stream().anyMatch(doc -> doc.isOk() == null || !doc.isOk());
+					if (someFailed) {
+						// logging not needed - user can enable `errors.log.enable` if required
+						for (int i = 0; i < writeResults.size(); i++) {
+							DocumentResult writeResult = writeResults.get(i);
+							if (writeResult.isOk() == null || !writeResult.isOk()) {
+								reporter.report(accumulatedSinkRecords.get(b * batchSize + i),
+										new RuntimeException(String.format("Failed to batch write document to Cloudant with error %s reason %s",
+												writeResult.getError(), writeResult.getReason())));
+							}
+						}
 					}
 				}
-			}
-		} catch (RuntimeException re) {
-			LOG.error(String.format("Exception thrown when trying to write documents: %s", re.getMessage()));
-			if (reporter != null) {
-				if (accumulatedSinkRecords != null) {
-					// all failed
-					for (SinkRecord r : accumulatedSinkRecords) {
-						reporter.report(r, re);
-					}
-				}
-			} else {
+
+				// if we got here, then the batch operation succeeded (ie no network failure and 2xx response)
+				// therefore we can clear out the accumulated sink records
+				// any individual failures reported back from the response to batch write will have been reported
+				// and potentially logged and/or sent to dlq if the user configured these
+				accumulatedSinkRecords = null;
+			} catch (RuntimeException re) {
+				// below we re-wrap the exception as a nicety - it's not required, see explanation below
+
+				// WorkerSinkTask#commitOffsets will catch any Throwable and will not advance the offsets, meaning
+				// that everything outstanding in accumulatedSinkRecords (and potentially more if put is called again)
+				// will be attempted to be re-written
+
+				// logging not needed - WorkerSinkTask#onCommitCompleted will log error including the below message
 				throw new ConnectException("Exception thrown when trying to write documents", re);
 			}
-		} finally {
-			accumulatedSinkRecords = null;
 		}
 	}
 	
